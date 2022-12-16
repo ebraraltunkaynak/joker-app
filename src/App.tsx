38036ab6795f7d5545b4ea5bfffbdbee1b7d3d74
Ebrar <16155052@mersin.edu.tr>
2022-12-17 02:01:07 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Wrapper,Row,Header,Image,Form,Search} from './components/styled/index'
+import joker from "./images/jok.png"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper>
+        <Row>
+          <Header>Joker</Header>
+          <Image src={joker} alt="Baykus" />
+        </Row>
+
+      <Form onSubmit={}>
+        
+        <Search type="text"
+        placeholder='Search' 
+        value={}
+         onChange={handleChange} />
+        </Form>  
+
+      </Wrapper>
+     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+// 33.24 
---
+++ src/components/styled/index.ts
@@ -0,0 +1,7 @@
+import Wrapper from "./Wrapper";
+import Header from "./Header";
+import Image from "./Image";
+import Row from "./Row";
+import Form from "./Form";
+import Search from "./Search";
+export { Wrapper,Row,Header,Image,Form,Search}
